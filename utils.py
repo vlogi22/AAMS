e781b3ab1c1a4d08cce8af76557fd2b51a1447eb
Luis <luis200202@hotmail.com>
2024-05-01 19:45:49 +0100

    fix(utils): plot function

--- utils.py
+++ utils.py
@@ -4,12 +4,12 @@ from typing import Optional, Sequence
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(n_ep, scores, filename="image", colors=None):
+def plot(n_ep, scores, image="image", colors=None):
     plt.clf()
     plt.xlabel('Episodes')
     plt.ylabel('Score')
     plt.plot(np.arange(1, n_ep + 1), scores)
-    plt.savefig(filename, bbox_inches = 'tight')
+    plt.savefig(image, bbox_inches = 'tight')
 
 
 
