05f5b1c8fba90f322f71ff71af3dbd99ed417ce7
Luis <luis200202@hotmail.com>
2024-05-06 18:45:24 +0100

    style: identation

--- agent/dqnAgent.py
+++ agent/dqnAgent.py
@@ -5,27 +5,27 @@ from agent.dqn import DQN
 
 class DQNAgent(BasicAgent):
 
-    def __init__(self, agentId, device: str = "cpu"):
-      super(DQNAgent, self).__init__(f"DQNAgent")
-      self.device_ = device
-      self.agentId_ = agentId
-      self.nActions_ = 4
-      self.brain_ = DQN(outputLayer = self.nActions_, device=device)
+  def __init__(self, agentId, device: str = "cpu"):
+    super(DQNAgent, self).__init__(f"DQNAgent")
+    self.device_ = device
+    self.agentId_ = agentId
+    self.nActions_ = 4
+    self.brain_ = DQN(outputLayer = self.nActions_, device=device)
 
-    def action(self) -> int:
-      #print("Qs ", self.brain_.getQs(self.observation/255))
-      return np.argmax(self.brain_.getQs(self.observation/255))
-    
-    def updateReplay(self, obs, action, reward, newObs, done):
-      self.brain_.updateReplay(obs, action, reward, newObs, done)
+  def action(self) -> int:
+    #print("Qs ", self.brain_.getQs(self.observation/255))
+    return np.argmax(self.brain_.getQs(self.observation/255))
+  
+  def updateReplay(self, obs, action, reward, newObs, done):
+    self.brain_.updateReplay(obs, action, reward, newObs, done)
 
-    def train(self, done, step):
-      return self.brain_.train(done, step)
+  def train(self, done, step):
+    return self.brain_.train(done, step)
 
-    def save(self, fileName='model.pth'):
-      print("agent", self.agentId_, " saving ", fileName)
-      self.brain_.save(fileName)
+  def save(self, fileName='model.pth'):
+    print("agent", self.agentId_, " saving ", fileName)
+    self.brain_.save(fileName)
 
-    def load(self, fileName='model.pth'):
-      print("agent", self.agentId_, " loading ", fileName)
-      self.brain_.load(fileName)
+  def load(self, fileName='model.pth'):
+    print("agent", self.agentId_, " loading ", fileName)
+    self.brain_.load(fileName)
