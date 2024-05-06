ca589352cf93bca9628c2822acec3aa25df8f7d7
Luis <luis200202@hotmail.com>
2024-05-06 18:50:16 +0100

    refactor(plots): change to scatters

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def plot(n_ep, scores, image="image", colors=None):
     plt.clf()
     plt.xlabel('Episodes')
     plt.ylabel('Score')
-    plt.plot(np.arange(1, n_ep + 1), scores)
+    plt.scatter(np.arange(1, n_ep + 1), scores, s=1)
     plt.savefig(image, bbox_inches = 'tight')
 
 
