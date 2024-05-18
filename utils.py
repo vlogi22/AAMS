3604cf5f3577d87a5242a543fe30e1dcb8b81cbc
Vlogi <luis200202@hotmail.com>
2024-05-18 11:48:57 +0100

    refactor(utils): add custom labels

--- utils.py
+++ utils.py
@@ -4,12 +4,12 @@ from typing import Optional, Sequence
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot(n_ep, scores, ylim=(-20,20), image="image", colors=None):
+def plot(xLen = 0, x = [], xLabel = 'X_Label', yLabel = 'Y_Label', ylim=(-20,20), s=1, image="image", colors=None):
   plt.clf()
-  plt.xlabel('Episodes')
-  plt.ylabel('Score')
+  plt.xlabel(xLabel)
+  plt.ylabel(yLabel)
   plt.ylim(ylim)
-  plt.scatter(np.arange(1, n_ep + 1), scores, s=0.1)
+  plt.scatter(np.arange(1, xLen + 1), x, s=s)
   plt.savefig(image, bbox_inches = 'tight')
 
 
