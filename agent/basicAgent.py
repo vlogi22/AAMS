00f2b7989addbba0308e47155a8a2857824fde17
Vlogi <luis200202@hotmail.com>
2024-05-18 11:47:04 +0100

    feat(basicAgent): add getters and setters

--- agent/basicAgent.py
+++ agent/basicAgent.py
@@ -20,23 +20,33 @@ class BasicAgent(ABC):
     self.obs_ = observation
     self.obsInfo_ = obsInfo
 
-  def name(self) -> str:
+  def getName(self) -> str:
     return self.name_
+  def setName(self, name: str):
+    self.name_ = name
   
-  def id(self) -> int:
+  def getId(self) -> int:
     return self.agentId_
+  def setId(self, id: int) -> int:
+    self.agentId_ = id
   
-  def maxEnergy(self) -> float:
+  def getMaxEnergy(self) -> float:
     return self.maxEnergy_
+  def setMaxEnergy(self, maEnergy: float) -> float:
+    self.maxEnergy_ = maEnergy
   
-  def energy(self) -> float:
+  def getEnergy(self) -> float:
     return self.energy_
-  
+  def setEnergy(self, energy) -> float:
+    self.energy_ = energy
   def resetEnergy(self):
     self.energy_ = self.maxEnergy_
 
-  def strength(self) -> float:
+  def getStrength(self) -> float:
     return self.strength_
-  
+  def addStrength(self, strength: float) -> float:
+    if (self.strength_ + strength >= 0.1):
+      self.strength_ += strength
+    
   def rgbArray(self) -> tuple:
     return (R + int(255*self.strength_), G, B)
